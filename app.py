1095449e475c666cb50f226dd47f84af2397ea21
OtisDing <93888172+OtisDing@users.noreply.github.com>
2022-02-01 16:45:54 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+import flask
+import io
+import string
+import time
+import os
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from flask import Flask, jsonify, request
+
+def prepare_image(img):
+    img = Image.open(io.BytesIO(img))
+    img = img.resize((224, 224))
+    img = np.array(img)
+    img = np.expand_dims(img, 0)
+    return img
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def infer_image():
+    # Catch the image file from a POST request
+    if 'file' not in request.files:
+        return "Please try again. The Image doesn't exist"
+
+    file = request.files.get('file')
+
+    if not file:
+        return
+
+    # Read the image
+    img_bytes = file.read()
+
+    # Prepare the image
+    img = prepare_image(img_bytes)
+
+    # Return on a JSON format
+    #return jsonify(prediction=predict_result(img))
