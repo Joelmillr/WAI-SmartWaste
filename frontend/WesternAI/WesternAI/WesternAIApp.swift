b0f0e14dae4bc7a7a6232139c779da053a4565b5
kravich2 <kravich2@uwo.ca>
2022-02-14 15:31:56 -0500

    frontendstart

---
+++ frontend/WesternAI/WesternAI/WesternAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WesternAIApp.swift
+//  WesternAI
+//
+//  Created by Keerthen Ravichandran on 2022-02-14.
+//
+
+import SwiftUI
+
+@main
+struct WesternAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
