c239f69687229097dfc7dba808aed4cc4a14b27c
kravich2 <kravich2@uwo.ca>
2022-02-23 17:54:08 -0500

    More Front End Work

--- frontend/WesternAI/WesternAI/ContentView.swift
+++ frontend/WesternAI/WesternAI/ContentView.swift
@@ -1,25 +1,18 @@
 //
-//  ContentView.swift
+// ContentView.swift
 //  Western AI App
-//
+//https://www.pinterest.ca/pin/799529740090720623/
 //  Created by Keerthen Ravichandran on 2022-02-08.
-//
+/*https://www.vectorstock.com/royalty-free-vector/picture-icon-image-symbol-vector-20419048*/
+//https://www.123rf.com/photo_139509263_stock-vector-blue-line-trash-can-icon-isolated-on-blue-background-garbage-bin-sign-recycle-basket-icon-office-tra.html
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         
-        ZStack {
-            Color(red: 217/255, green: 235/255, blue: 255/255)
-                .edgesIgnoringSafeArea(.all)
-            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-            }
-        }
         
         ZStack {
-            
           
             Color(red: 217/255, green: 235/255, blue: 255/255)
                             .edgesIgnoringSafeArea(.all)
@@ -41,9 +34,7 @@ struct ContentView: View {
             Image("ClosedTrashCan")
                 .resizable()
                 .scaledToFit()
-             
-            
-            
+    
             Text("Take photo or upload photo to scan")
                 .font(.title2)
                 .fontWeight(.bold)
@@ -55,23 +46,18 @@ struct ContentView: View {
             
 
             
-            /*.frame(width:220,height: 220)*/
+            /*.frame(width:220,height: 220)
+             
+             Image("CameraLogo")
+                 .resizable()
+                 .scaledToFit().position(x: 40, y:720).frame(width:295)*/
         }
-        
-            
-           
-        
-        
     }
-    
-   
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
 .previewInterfaceOrientation(.portrait)
-        
     }
 }
