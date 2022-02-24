84ab867f785a1e2aaa523f8653c7f781351e102c
kravich2 <kravich2@uwo.ca>
2022-02-23 20:21:16 -0500

    More Front End Work

---
+++ frontend/WesternAI/WesternAI/CameraPicker.swift
@@ -0,0 +1,55 @@
+//
+//  ImagePicker.swift
+//  SwiftUIImagePicker
+//
+//  Created by Simon Ng on 10/6/2020.
+//  Copyright © 2020 AppCoda. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+struct ImagePicker2: UIViewControllerRepresentable {
+    
+    var sourceType: UIImagePickerController.SourceType = .camera
+    
+    @Binding var selectedImage: UIImage
+    @Environment(\.presentationMode) private var presentationMode
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker2>) -> UIImagePickerController {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = context.coordinator
+        
+        return imagePicker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker2>) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        
+        var parent: ImagePicker2
+        
+        init(_ parent: ImagePicker2) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+            
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
+
--- frontend/WesternAI/WesternAI/ContentView.swift
+++ frontend/WesternAI/WesternAI/ContentView.swift
@@ -9,55 +9,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShowPhotoLibrary=false
+    @State private var image = UIImage()
+    
     var body: some View {
         
         
         ZStack {
-          
+            
             Color(red: 217/255, green: 235/255, blue: 255/255)
-                            .edgesIgnoringSafeArea(.all)
+                .edgesIgnoringSafeArea(.all)
             Image("CameraLogo")
                 .resizable()
                 .scaledToFit().position(x: 40, y:720).frame(width:295)
             
-            Button("Camera Button ") {
-                
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-            }.position(x: 90, y: 720)
             
             Text("Eco Force")
-                            .font(.largeTitle)
-                            .fontWeight(.semibold)
-                            .multilineTextAlignment(.center)
-                            .padding([.top, .trailing]).position(x: 200, y:80)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .multilineTextAlignment(.center)
+                .padding([.top, .trailing]).position(x: 200, y:80)
             
             Image("ClosedTrashCan")
                 .resizable()
                 .scaledToFit()
-    
+            
             Text("Take photo or upload photo to scan")
                 .font(.title2)
                 .fontWeight(.bold)
-                            .multilineTextAlignment(.center)
-                            .padding([.top, .trailing]).position(x:200, y:610)
+                .multilineTextAlignment(.center)
+                .padding([.top, .trailing]).position(x:200, y:610)
+            
+            
             Image("Photo")
                 .resizable()
                 .scaledToFit().position(x: 150, y:720).frame(width:70)
             
-
+           
             
-            /*.frame(width:220,height: 220)
-             
-             Image("CameraLogo")
-                 .resizable()
-                 .scaledToFit().position(x: 40, y:720).frame(width:295)*/
-        }
+           /* Button("    Camera     ") {
+                self.isShowPhotoLibrary = true
+            }.position(x: 90, y: 720)
+        .sheet(isPresented: $isShowPhotoLibrary){
+            CameraPicker(sourceType: .camera, selectedImage: self.$image)
+        }*/
+            Button("                ") {
+                 self.isShowPhotoLibrary = true
+             }.position(x: 300, y: 720)
+         .sheet(isPresented: $isShowPhotoLibrary){
+             ImagePicker(sourceType: .photoLibrary, selectedImage: self.$image)
+         }
+            
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-.previewInterfaceOrientation(.portrait)
+            .previewInterfaceOrientation(.portrait)
     }
+}
 }
